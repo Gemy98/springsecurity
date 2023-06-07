3b612a8fd05d933058da3cbc59abf7ea28fd8e87
Gemy98 <mohamedgamal19981998@gmail.com>
2023-06-07 11:25:49 +0300

    add spring dependency and thymleaf security dependency #1

---
+++ src/main/java/com/spring/springsecurity/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.spring.springsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("api/")
+// http://localhost:8080/api/
+public class LoginController {
+
+   // http://localhost:8080/api/login
+    @GetMapping("login")
+    public String login(){
+        return "login";
+    }
+
+
+
+}
---
+++ src/main/java/com/spring/springsecurity/controller/MainController.java
@@ -0,0 +1,49 @@
+package com.spring.springsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("api/")
+// http://localhost:8080/api/
+public class MainController {
+
+
+    // http://localhost:8080/api/main
+    @GetMapping("main")
+    public String index(){
+        return "index";
+    }
+
+
+    // http://localhost:8080/api/fr
+    @GetMapping("fr")
+    public String fr(){
+        return "fragments";
+    }
+
+
+    // http://localhost:8080/api/profile
+    @GetMapping("profile")
+    public String profile(){
+        return "profile/index";
+    }
+
+
+    // http://localhost:8080/api/manage
+    @GetMapping("manage")
+    public String manage(){
+        return "management/index";
+    }
+
+
+
+    // http://localhost:8080/api/admin
+    @GetMapping("admin")
+    public String admin(){
+        return "admin/index";
+    }
+
+
+}
