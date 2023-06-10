3343a86b65e36b5a32aabaa96bceeb66035fb10a
Gemy98 <mohamedgamal19981998@gmail.com>
2023-06-10 07:16:05 +0300

    hibernate and JPA with models (create models Entity and relationships) closes #8

--- src/main/java/com/spring/springsecurity/model/Authorities.java
+++ src/main/java/com/spring/springsecurity/model/Authorities.java
@@ -1,14 +1,23 @@
 package com.spring.springsecurity.model;
 
+import jakarta.persistence.*;
+
 import java.util.Set;
 
+@Entity
+@Table(name="authorities")
 public class Authorities {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id ;
 
+    @Column(name ="authorities_name")
     private String authoritiesName ;
 
 
+
+    @ManyToMany(mappedBy = "authorities")
     private Set<User> users ;
 
 
--- src/main/java/com/spring/springsecurity/model/Role.java
+++ src/main/java/com/spring/springsecurity/model/Role.java
@@ -1,14 +1,26 @@
 package com.spring.springsecurity.model;
 
+import jakarta.persistence.*;
+
 import java.util.Set;
 
+@Entity
+@Table(name = "roles")
 public class Role {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id ;
 
+
+
+    @Column(name ="role_name")
     private String roleName ;
 
 
+
+
+    @ManyToMany( mappedBy = "roles")
     private Set<User> users ;
 
 
